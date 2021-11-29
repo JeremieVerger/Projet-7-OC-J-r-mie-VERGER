ec0fe2e92ad60943730980460e6da550239c7ed5
Jeremie Verger <jeremie.verger@outlook.fr>
2021-11-29 19:02:02 +0100

    Tentative d'extraction du fichier zip via la librairie patool

--- appli/pipeline.py
+++ appli/pipeline.py
@@ -9,12 +9,12 @@ from sklearn import manifold, decomposition
 from sklearn import preprocessing
 import joblib
 from sklearn import linear_model
-import shutil
+import patoolib
 from six.moves import urllib
 
 # on récupère puis décompresse les fichiers CSV contenant les données
 urllib.request.urlretrieve('https://s3-eu-west-1.amazonaws.com/static.oc-static.com/prod/courses/files/Parcours_data_scientist/Projet+-+Impl%C3%A9menter+un+mod%C3%A8le+de+scoring/Projet+Mise+en+prod+-+home-credit-default-risk.zip', "Data.zip")
-shutil.unpack_archive('Data.zip', 'P7_data')
+patoolib.extract_archive("Data.zip", outdir="P7_data")
 
 # création d'une fonction de détection des valeurs manquantes
 
