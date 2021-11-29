d182a8ea1a5934b5446f78c99522957242c7afcc
Jeremie Verger <jeremie.verger@outlook.fr>
2021-11-29 17:33:00 +0100

    Tentative de correction de l'application pour qu'elle charge le dossier zip des données brutes à partir de l'url grâce à la librairie wget

--- appli/pipeline.py
+++ appli/pipeline.py
@@ -10,13 +10,12 @@ from sklearn import preprocessing
 import joblib
 from sklearn import linear_model
 from pyunpack import Archive
-import urllib.request
+import wget
 
 # on récupère puis décompresse les fichiers CSV contenant les données
-urllib.request.urlretrieve('https://s3-eu-west-1.amazonaws.com/static.oc-static.com/prod/courses/files/Parcours_data_scientist/Projet+-+Impl%C3%A9menter+un+mod%C3%A8le+de+scoring/Projet+Mise+en+prod+-+home-credit-default-risk.zip', "Data.zip")
-files = Archive("Data.zip",'r')
+url = "https://s3-eu-west-1.amazonaws.com/static.oc-static.com/prod/courses/files/Parcours_data_scientist/Projet+-+Impl%C3%A9menter+un+mod%C3%A8le+de+scoring/Projet+Mise+en+prod+-+home-credit-default-risk.zip"
+files = wget.download(url)
 files.extractall("P7_data")
-
 
 # création d'une fonction de détection des valeurs manquantes
 
